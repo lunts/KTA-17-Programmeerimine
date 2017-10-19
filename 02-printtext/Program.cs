277d8e019f98b7dc91a21e1b239f1f90a1c0eef1
lunts <32923383+lunts@users.noreply.github.com>
2017-10-19 14:10:04 +0300

    ex1-4

---
+++ 02-printtext/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_printtext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Minu nimi on (sisesta oma nimi) ning õpin esimest korda programmeerima!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Kindlasti on võimalik midagi rohkemat, kui lihtsalt teksti printimine \n");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04-Parse-Input-to-int/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Parse_Input_to_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja ma ütlen Sulle nende summa");
+            Console.WriteLine();
+
+            Console.Write("Esimene number: ");
+
+            string input1 = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Teine number: ");
+            string input2 = Console.ReadLine();
+            Console.WriteLine();
+
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+          
+
+            Console.Write("Tulemuseks on: " + (number1 + number2));
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
