17858a792fb799f69c82771560a21bc4bd67ff54
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 15:46:57 +0200

    Uuendasin faile

---
+++ Excercises/Excercises/Excuses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excuses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vabanduste genereerimise programm
+            //Sisesta komadega eraldatud vabandused
+            //>koer sõi ära, ema ei lubanud arvutis olla, ei viitsinud
+            //Tänane vabandus on: ei viitsinud
+            //Split
+
+
+            Console.WriteLine("Sisesta komadega eraldatud vabandused.");
+            Console.WriteLine();
+            string input = Console.ReadLine();
+
+            string[] massiiv = input.Split(',');
+
+            int random = new Random().Next(0, massiiv.Length);
+
+            Console.WriteLine($"Tänane vabandus on: {massiiv[random]}");
+
+            Console.ReadLine();
+
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            //Console.ReadLine();
+
+            //int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            //    IEnumerable<int> scoreQuery =
+            //        from score in scores
+            //        where score > 80
+            //        select score;
+
+            //// Execute the query.
+            //foreach (int i in scoreQuery)
+            //{
+            //    Console.Write(i + " ");
+            //}
+        }
+    }
+}
--- Excercises/Excercises/ResearchArrays/Program.cs
+++ Excercises/Excercises/ResearchArrays/Program.cs
@@ -10,6 +10,26 @@ namespace ResearchArrays
     {
         static void Main(string[] args)
         {
+            //1. Google and try to understand what are arrays
+            //2. Create small illustrative example
+            //3. Do NOT copy & paste, use learnt concepts to create your own
+            //4. Create pull request
+            //5. Share with friends
+
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
