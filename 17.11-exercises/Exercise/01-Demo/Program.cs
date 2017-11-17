bb5a8f124fc06c825cf67d098d3113ca1a9311c5
lunts <32923383+lunts@users.noreply.github.com>
2017-11-17 13:54:16 +0200

    create new exercise

---
+++ 17.11-exercises/Exercise/01-Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olen demoprogramm");
+
+            Console.WriteLine("Press any key to continue.");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
