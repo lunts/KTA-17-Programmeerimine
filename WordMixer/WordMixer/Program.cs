3ec221954d6c428e47ae302b6aa9e855e97580f8
lunts <32923383+lunts@users.noreply.github.com>
2018-02-12 12:57:03 +0200

    tunnikontroll 12.02.2018
    tunnikontroll 12.02.2018

---
+++ WordMixer/WordMixer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordMixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("See programm jätab lauses iga sõna esimese ja viimase tähe paigale, kuid muudab sõna sees olevate tähtede järjekorda");
+
+            Console.WriteLine("Kirjuta lause ilma kirjavahemärkideta: ");
+
+            
+            string input = Console.ReadLine();
+            List<string> input = new List<string>(Console.ReadLine());
+            string[] sentence = input.ToArray();
+
+            //sõna toimib
+            string unsuffled = Console.ReadLine();
+            char[] charArr = unsuffled.ToCharArray();
+            
+          
+            var rnd = new Random();
+            int middleStart = 1;
+            int middleLength = unsuffled.Length - 2;
+            string middle = unsuffled.Substring(middleStart, middleLength);
+                      
+            string shuffled = new string(middle.OrderBy(r => rnd.Next()).ToArray());
+            Console.WriteLine(charArr[0] + shuffled + charArr[charArr.Length - 1]);
+            //https://stackoverflow.com/questions/3226877/character-shuffler
+
+            
+            Console.ReadKey();
+        }
+    }
+}
